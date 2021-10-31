46b484efbbddbc7e9da19c47c95415aaded96696
oldkingcone <11233163+oldkingcone@users.noreply.github.com>
2021-10-31 19:21:47 -0400

    Add files via upload

---
+++ lib/database/bettersploitdb.py
@@ -0,0 +1,146 @@
+import os
+import string
+import secrets
+import psycopg2
+
+
+class BetterDatabase:
+	genUserList = []
+	checkDB = True
+	verifyUser = True
+	multiUser = True
+	dbaseName = "bettersploit_main"
+	dbaseUser = "postgres"
+	dbasePassword = ""
+	dbasePort = 5432
+	dbaseHost = "localhost"
+	connectionString = psycopg2.connect(
+		dbname=dbaseName,
+		user=dbaseUser,
+		password=dbasePassword,
+		host=dbaseHost
+	)
+	cursor = connectionString.cursor()
+
+	def __init__(self):
+		self.genUserList = BetterDatabase.genUserList
+		self.dbaseHost = BetterDatabase.dbaseHost
+		self.dbasePort = BetterDatabase.dbasePort
+		self.dbaseUser = BetterDatabase.dbaseUser
+		self.dbasePassword = BetterDatabase.dbasePassword
+		self.dbaseName = BetterDatabase.dbaseName
+		self.checkDB = BetterDatabase.checkDB
+		self.verifyUser = BetterDatabase.verifyUser
+		self.multiUser = BetterDatabase.multiUser
+		self.cursor = BetterDatabase.cursor
+
+	def createdb(self, first_run=bool(), userlist=""):
+		if first_run is True:
+			self.cursor.execute('''
+				create table if not exists bettersploit_sploits
+				(
+					id         serial                                             not null
+						constraint bettersploit_sploits_pkey
+							primary key,
+					datetime   timestamp with time zone default CURRENT_TIMESTAMP not null,
+					version    text,
+					cve        text
+						constraint bettersploit_sploits_cve_key
+							unique,
+					path       text
+						constraint bettersploit_sploits_path_key
+							unique,
+					desciption text                                               not null
+				);
+				alter table bettersploit_sploits
+					owner to bettersploit;
+			
+			create table if not exists public.bettersploit_log
+			(
+				id                   serial not null
+											constraint bettersploit_log_pk
+											primary key,
+				bettersploit_user          text   not null,
+				bettersploit_function_used text,
+				target               text,
+				where_is_result      text
+			);
+			alter table public.bettersploit_log
+				owner to bettersploit;
+			create table if not exists public.bettersploit_tools
+			(
+				id               serial                                             not null
+					constraint bettersploit_tools_pkey
+						primary key,
+				datetime         timestamp with time zone default CURRENT_TIMESTAMP not null,
+				lang             text,
+				path             text
+					constraint bettersploit_tools_path_key
+						unique,
+				types             text,
+				purpose          text
+			);
+			alter table public.bettersploit_tools
+				owner to bettersploit;
+			create table if not exists public.bettersploit_loots
+			(
+				id               serial                                             not null
+					constraint bettersploit_loots_pkey
+						primary key,
+				datetime         timestamp with time zone default CURRENT_TIMESTAMP not null,
+				operating_system text,
+				host             text,
+				local_path       text,
+				type_of_loot     text,
+				persist          boolean,
+				best_cve         text,
+				used_cve         text
+			);
+			alter table public.bettersploit_loots owner to bettersploit;
+			create table if not exists public.bettersploit_data
+			(
+				id       serial                                             not null
+					constraint bettersploit_data_pkey
+						primary key,
+				dtg      timestamp with time zone default CURRENT_TIMESTAMP not null,
+				when_run text
+			);
+			alter table public.bettersploit_data
+				owner to bettersploit; 
+			create table if not exists public.bettersploit_evaders
+			(
+				id             serial                              not null,
+				dategroup      timestamp default CURRENT_TIMESTAMP not null,
+				evadername     text,
+				evaderdoes     text,
+				evadercommands text,
+				evaderpath     text,
+				evaderfulldesc text
+			);
+			
+			alter table bettersploit_evaders
+				owner to bettersploit;
+			
+			create unique index bettersploit_evaders_evadercommands_uindex
+				on bettersploit_evaders (evadercommands);
+			
+			create unique index bettersploit_evaders_evadername_uindex
+				on bettersploit_evaders (evadername);
+			
+			create unique index bettersploit_evaders_evaderpath_uindex
+				on bettersploit_evaders (evaderpath);
+			'''
+			)
+		if isinstance(userlist, list):
+			pw = ''
+			digits = string.digits
+			asciilower = string.ascii_lowercase
+			asciiupper = string.ascii_uppercase
+			all = asciiupper + asciilower + digits
+			for item in userlist:
+				for _ in range(15):
+					pw += secrets.choice(all)
+				self.cursor.execute(f"CREATE ROLE {item} WITH LOGIN ENCRYPTED PASSWORD '{pw}'")
+				self.cursor.execute(f"GRANT SELECT,INSERT,UPDATE ON ")
+				self.genUserList.append(f"{item}:{pw}")
+
